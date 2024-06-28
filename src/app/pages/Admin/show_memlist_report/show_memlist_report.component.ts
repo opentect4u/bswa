97ab64dbd49737332ab64bb3f80af26e4515c9e3
Utsab Roy <roy.utsab01@gmail.com>
2024-06-28 16:41:53 +0530

    fixed print and home page design

--- src/app/pages/Admin/show_memlist_report/show_memlist_report.component.ts
+++ src/app/pages/Admin/show_memlist_report/show_memlist_report.component.ts
@@ -62,9 +62,27 @@ export class Show_memlist_reportComponent implements OnInit {
       '<html><head><title></title><style type="text/css">'
     );
 
-    this.WindowObject.document.writeln(
-      '@media print {.wraper { margin-left: 10px !important; margin-right: 10px !important; } .contant-wraper { border-top: 2px solid #32678d; background-color: white; margin-bottom: 100px; overflow: auto; }</style>'
-    );
+    // this.WindowObject.document.writeln(
+    //   '@media print {.wraper { margin-left: 10px !important; margin-right: 10px !important; } .contant-wraper { border-top: 2px solid #32678d; background-color: white; margin-bottom: 100px; overflow: auto; }</style>'
+    // );
+
+    this.WindowObject.document.writeln('@media print { .center { text-align: center;}' +
+      'body{font-family:Arial, Tahoma, Verdana;font-size: 14px;color: #6f7479;}' +
+      '.wrapper{box-shadow: none !important; max-width: 1100px; width: 100%; margin: 0 auto; font-family:Arial, Tahoma, Verdana;}' +
+      '.contant-wraper{box-shadow: none !important;}'+
+      // '.table_print_outer {border:red solid 2px;}' +
+      '.table_print_outer table thead tr th{background:#000 !important; color:#fff; margin: 0 !important; padding:7px 3px; border: none;}' +
+      '.table_print_outer table thead{text-align: left; background:#000 !important;}' +
+      '.table_print_outer table tbody tr td{text-align: left; text-wrap: wrap; color:#333; padding:7px 3px; word-break: break-word; border: none; margin: 0 !important;}' +
+      '.table_print_outer table tbody tr{border-bottom:#333 solid 1px;}' +
+      '.print_top_head h2{margin: 0; padding: 0; font-size:20px; color:#000;}' +
+      '.print_top_head h4{margin: 0; padding: 0; font-size:16px; color:#000;}' +
+      '.print_top_Title h4{margin: 0; padding: 0; font-size:16px; color:#000;}' +
+      '.msg_adress{width:120px;}'+
+      '.table_head_cus tr td{background: #D9D9D9;}' +
+              '} </style>');
+
+
     this.WindowObject.document.writeln(
       '<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">'
     );
