e3472cdcffab2831f1950fe437af3e22fbc238d5
Subham <subham@synergicsoftek.in>
2024-07-01 02:44:41 +0530

    Member form edit and view 01.07.2024

---
+++ src/app/pages/Admin/member_details/member-edit/member-edit.component.ts
@@ -0,0 +1,323 @@
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+// import { MessageService } from 'primeng/api/messageservice';
+import { DataService } from 'src/app/service/data.service';
+import { ValidatorsService } from 'src/app/service/validators.service';
+import { DatePipe } from '@angular/common';
+import Swal from 'sweetalert2';
+import { MessageService } from 'primeng/api';
+import { environment } from 'src/environments/environment';
+
+interface UserInfo {
+  form_no: string;
+  form_dt: string;
+  member_id: string;
+  mem_dt: string;
+  mem_type: string;
+  memb_oprn: string;
+  memb_name: string;
+  unit_id: string;
+  gurdian_name: string;
+  gender: string;
+  marital_status: string;
+  dob: string;
+  blood_grp: string;
+  caste: string;
+  staff_nos: string;
+  pers_no: string;
+  min_no: string;
+  memb_address: string;
+  ps: string;
+  city_town_dist: string;
+  pin_no: string;
+  phone_no: string;
+  email_id: string;
+  memb_pic: string;
+  memb_status: string;
+  remarks: string;
+  resolution_no: string;
+  resolution_dt: string;
+  spou_dt: DepInfo;
+  dep_dt: [DepInfo];
+}
+
+interface DepInfo {
+  form_no: string;
+  sl_no: string;
+  member_id: string;
+  mem_type: string;
+  dependent_dt: string;
+  dependent_name: string;
+  gurdian_name: string;
+  relation: string;
+  min_no: string;
+  dob: string;
+  blood_grp: string;
+  memb_address: string;
+  ps: string;
+  city_town_dist: string;
+  pin_no: string;
+  phone_no: string;
+  email_id: string;
+  memb_pic: string;
+  intro_member_id: string;
+  dept_status: string;
+  grp_status: string;
+  grp_no: string;
+  stp_status: string;
+  stp_no: string;
+}
+
+@Component({
+  selector: 'app-member-edit',
+  templateUrl: './member-edit.component.html',
+  styleUrls: ['./member-edit.component.css'],
+  providers: [DatePipe, MessageService],
+})
+export class MemberEditComponent implements OnInit {
+  form_no: any;
+
+  form!: FormGroup;
+  responsedata: any;
+  userData: any;
+
+  memberData: UserInfo | undefined;
+  responsedata_rel: any;
+  uploadedFiles: any[] = [];
+  spouseFile: any = null
+  memFile: any = null
+
+  api_url = environment.api_url
+  spouseFileName: any = null;
+  memFileName: any = null;
+
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private dataServe: DataService,
+    private validatorsService: ValidatorsService,
+    private route: ActivatedRoute,
+    private datePipe: DatePipe
+  ) {}
+
+  ngOnInit() {
+    const encodedFormNo = this.route.snapshot.params['form_no'];
+    // this.form_no = ato b(decodeURIComponent(encodedFormNo));
+    this.form_no = atob(decodeURIComponent(encodedFormNo));
+
+    this.form = this.fb.group({
+      form_no: [this.form_no],
+      member_type: [{ value: 'Genaral Membership', disabled: true }],
+      member_opt: ['', Validators.required],
+      unit_nm: ['', Validators.required],
+      member: ['', Validators.required],
+      gurdian: ['', Validators.required],
+      gen: ['', Validators.required],
+      marital_status: [''],
+      gen_dob: ['', Validators.required],
+      phone: ['', Validators.required],
+      email_id: [''],
+      blood: ['', Validators.required],
+      caste: [''],
+      staff: ['', Validators.required],
+      personal: ['', Validators.required],
+      min: [''],
+      mem: ['', Validators.required],
+      city: [''],
+      pin: ['', Validators.required],
+      police_st: [''],
+      spouse_fr: this.fb.group({
+        spou_name: [''],
+        spou_gurd_name: [''],
+        spou_blood_grp: [''],
+        spou_dob: [''],
+        // spou_phone: [''],
+        spou_mobile_no: [''],
+        spou_min_no: [''],
+        spou_mem_addr: [''],
+        spou_police_st: [''],
+        spou_city: [''],
+        sl_no: [0],
+      }),
+      depenFields: this.fb.array([]),
+      user: [localStorage.getItem('user_name')],
+      mem_type: [''],
+    });
+
+    this.unit();
+    this.relationship();
+    this.getMemberDetails();
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  get depenFields(): FormArray {
+    // console.log((<FormArray>this.form.get('depenFields')).controls, 'GET');
+    return this.form.get('depenFields') as FormArray;
+  }
+
+  unit() {
+    this.dataServe
+      .global_service(0, '/master/unit_list', null)
+      .subscribe((data: any) => {
+        this.responsedata = data;
+        console.log(this.responsedata, '555');
+        this.responsedata =
+          this.responsedata.suc > 0 ? this.responsedata.msg : [];
+      });
+  }
+
+  relationship() {
+    this.dataServe
+      .global_service(0, '/master/relationship_list', null)
+      .subscribe((data: any) => {
+        this.responsedata_rel = data;
+        console.log(this.responsedata_rel);
+        this.responsedata_rel =
+          this.responsedata_rel.suc > 0 ? this.responsedata_rel.msg : [];
+      });
+  }
+
+  getMemberDetails() {
+    this.dataServe
+      .global_service(1, '/member_dtls', { flag: true, form_no: this.form_no })
+      .subscribe(
+        (data) => {
+          console.log(data, 'kiki');
+          this.userData = data;
+          // this.userData = this.userData.msg;
+          if (this.userData.suc > 0) {
+            this.memberData = this.userData.msg[0];
+            this.form.patchValue({
+              mem_type: this.memberData?.mem_type,
+              member_type:
+                this.memberData?.mem_type == 'G'
+                  ? 'General Membership'
+                  : this.memberData?.mem_type == 'L'
+                  ? 'Life Membership'
+                  : 'Associate Membership',
+              member_opt: this.memberData?.memb_oprn,
+              unit_nm: this.memberData?.unit_id,
+              member: this.memberData?.memb_name,
+              gurdian: this.memberData?.gurdian_name,
+              gen: this.memberData?.gender,
+              marital_status: this.memberData?.marital_status,
+              gen_dob: this.memberData?.dob
+                ? this.datePipe.transform(this.memberData?.dob, 'yyyy-MM-dd')
+                : '',
+              phone: this.memberData?.phone_no,
+              email_id: this.memberData?.email_id,
+              blood: this.memberData?.blood_grp,
+              caste: this.memberData?.caste,
+              staff: this.memberData?.staff_nos,
+              personal: this.memberData?.pers_no,
+              min: this.memberData?.min_no,
+              mem: this.memberData?.memb_address,
+              city: this.memberData?.city_town_dist,
+              pin: this.memberData?.pin_no,
+              // police_st: this.memberData,
+              spouse_fr: {
+                sl_no: this.memberData?.spou_dt.sl_no,
+                spou_name: this.memberData?.spou_dt.dependent_name,
+                spou_gurd_name: this.memberData?.spou_dt.gurdian_name,
+                spou_blood_grp: this.memberData?.spou_dt.blood_grp,
+                spou_dob: this.memberData?.spou_dt.dob
+                  ? this.datePipe.transform(
+                      this.memberData?.spou_dt.dob,
+                      'yyyy-MM-dd'
+                    )
+                  : '',
+                // spou_phone: this.memberData?.spou_dt,
+                spou_mobile_no: this.memberData?.spou_dt.phone_no,
+                spou_min_no: this.memberData?.spou_dt.min_no,
+                spou_mem_addr: this.memberData?.spou_dt.memb_address,
+                spou_police_st: this.memberData?.spou_dt.ps,
+                spou_city: this.memberData?.spou_dt.city_town_dist,
+              },
+            });
+
+            for (let dt of this.memberData!.dep_dt) {
+              this.depenFields.push(
+                this.fb.group({
+                  sl_no: [dt.sl_no],
+                  dependent_name: [dt.dependent_name],
+                  phone_no: [dt.phone_no],
+                  relation: [dt.relation],
+                })
+              );
+            }
+
+            this.spouseFileName = this.memberData?.spou_dt.memb_pic ? `${this.api_url}/${this.memberData?.spou_dt.memb_pic}` : null
+            this.memFileName = this.memberData?.memb_pic ? `${this.api_url}/${this.memberData?.memb_pic}` : null
+          }
+          console.log(this.userData, 'lili');
+        },
+        (error) => {
+          console.error(error);
+          Swal.fire(
+            'Warning',
+            'An error occurred while fetching data',
+            'warning'
+          );
+          // this.messageService.add({ severity: 'error', summary: 'Error', detail: 'An error occurred while saving data' });
+        }
+      );
+  }
+
+  onUpload(event: any, flag: any) {
+    const file = event.files[0];
+    if (file) {
+      if(flag == 'O') this.memFile = file
+
+      if(flag == 'S') this.spouseFile = file
+      // this.fileSelected.emit({ file, flag });
+    }
+
+    // for (let file of event.files) {
+    //   this.uploadedFiles.push(file);
+    // }
+
+    // this.messageService.add({
+    //   severity: 'info',
+    //   summary: 'File Uploaded',
+    //   detail: '',
+    // });
+  }
+
+  onRemove(event: any, flag: any) {
+    if (flag == 'O') this.memFile = null;
+
+    if (flag == 'S') this.spouseFile = null;
+    console.log(event, 'clear event');
+    // this.fileSelected.emit({ file: '', flag });
+  }
+
+  save() {
+    const frmDt = new FormData();
+    frmDt.append('data', JSON.stringify(this.form.value))
+    frmDt.append('spouse_file', this.spouseFile)
+    frmDt.append('member_file', this.memFile) 
+    this.dataServe
+      .global_service(1, '/update_member_dtls', frmDt)
+      .subscribe((data) => {
+        this.responsedata = data;
+        if (this.responsedata.suc > 0) {
+          Swal.fire(
+            'Success!',
+            ` Your form is Updated successfully.`,
+            'success'
+          ).then((result) => {
+            if (result.isConfirmed) {
+              this.router.navigate(['/admin/print_form']);
+            }
+          });
+        } else {
+          Swal.fire('Error', this.responsedata.msg, 'error');
+        }
+      });
+    // console.log(this.form.value)
+  }
+}
--- src/app/pages/Admin/sidebar/sidebar.component.ts
+++ src/app/pages/Admin/sidebar/sidebar.component.ts
@@ -19,90 +19,95 @@ export class SidebarComponent implements OnInit {
   ngOnInit() {
     this.items = [
       {
-          label: 'Membership Forms',
-          icon: 'pi pi-user',
-          items: [
-              {
-                  label: 'Accept / Reject Membership Forms',
-                  icon: 'pi pi-hourglass',
-                  routerLink: ['admin_approve']
-              },
-            
-              {
-                  label: 'Approve Membership Forms',
-                  icon: 'pi pi-check-square',
-                  routerLink: ['approve_form']
-              }
-          ]
+        label: 'Membership Forms',
+        icon: 'pi pi-user',
+        items: [
+          {
+            label: 'Accept / Reject Membership Forms',
+            icon: 'pi pi-hourglass',
+            routerLink: ['admin_approve'],
+          },
+
+          {
+            label: 'Approve Membership Forms',
+            icon: 'pi pi-check-square',
+            routerLink: ['approve_form'],
+          },
+        ],
       },
       {
-          label: 'Super Topup Policy',
-          icon: 'pi pi-fw pi-bookmark',
-          items: [
-              {
-                  label: 'Accept / Reject Super Topup Policy Forms',
-                  icon: 'pi pi-search',
-                  routerLink: ['admin_premium_approve']
-              },
-              {
-                  label: 'Approve Super Topup Policy Form',
-                  icon: 'pi pi-check-circle',
-                  routerLink: ['super_policy_approve']
-              },
-          ]
+        label: 'Super Topup Policy',
+        icon: 'pi pi-fw pi-bookmark',
+        items: [
+          {
+            label: 'Accept / Reject Super Topup Policy Forms',
+            icon: 'pi pi-search',
+            routerLink: ['admin_premium_approve'],
+          },
+          {
+            label: 'Approve Super Topup Policy Form',
+            icon: 'pi pi-check-circle',
+            routerLink: ['super_policy_approve'],
+          },
+        ],
       },
       {
         label: 'Existing Group Policy',
         icon: 'pi pi-fw pi-users',
         items: [
-            {
-                label: 'Accept / Reject Group Policy Form',
-                icon: 'pi pi-check',
-                routerLink: ['admin_group_premium_approve']
-            },
-            {
-                label: 'Approve Group Policy Form',
-                icon: 'pi pi-verified',
-                routerLink: ['group_policy_approve_form']
-            },
-        ]
+          {
+            label: 'Accept / Reject Group Policy Form',
+            icon: 'pi pi-check',
+            routerLink: ['admin_group_premium_approve'],
+          },
+          {
+            label: 'Approve Group Policy Form',
+            icon: 'pi pi-verified',
+            routerLink: ['group_policy_approve_form'],
+          },
+        ],
       },
       {
         label: 'Subscription Deposit',
         icon: 'pi pi-fw pi-users',
         items: [
-            {
-                label: 'Entry Deposit',
-                icon: 'pi pi-check',
-                routerLink: ['subs_depo_entry']
-            },
-            {
-                label: 'Approve Deposit',
-                icon: 'pi pi-verified',
-                routerLink: ['subs_depo_approve']
-            },
-        ]
+          {
+            label: 'Entry Deposit',
+            icon: 'pi pi-check',
+            routerLink: ['subs_depo_entry'],
+          },
+          {
+            label: 'Approve Deposit',
+            icon: 'pi pi-verified',
+            routerLink: ['subs_depo_approve'],
+          },
+        ],
       },
       {
         label: 'Reports',
         icon: 'pi pi-id-card',
         items: [
-            {
-                label: 'Member Registered Report',
-                icon: 'pi pi-users',
-                routerLink: ['member_list_report']
-            },
-            {
-              label: 'Member Transaction Report',
-              icon: 'pi pi-money-bill',
-              routerLink: ['member_trans_report']
+          {
+            label: 'Member Register',
+            icon: 'pi pi-users',
+            routerLink: ['member_list_report'],
+          },
+          {
+            label: 'Member Transaction',
+            icon: 'pi pi-money-bill',
+            routerLink: ['member_trans_report'],
           },
           {
             label: 'Subscription Cleared Upto',
             icon: 'pi pi-undo',
-            routerLink: ['subscription_cleared_upto']
-        },
-        ]
+            routerLink: ['subscription_cleared_upto'],
+          },
+        ],
+      },
+      {
+        label: 'Member Details',
+        icon: 'pi pi-users',
+        routerLink: ['memb_list'],
       },
     ];
 }
