5c289cdc72ce58dfb7f73f69c22fce5b00ccffa3
Sayantika Dhar <sayantika.dhar04@gmail.com>
2024-08-30 10:37:58 +0530

    commit payment preview page 3.008.2024

--- src/app/pages/Auth/payment_preview_page/payment_preview_page.component.ts
+++ src/app/pages/Auth/payment_preview_page/payment_preview_page.component.ts
@@ -5,6 +5,8 @@ import { DataService } from 'src/app/service/data.service';
 import { MessageService } from 'primeng/api';
 import { ActivatedRoute } from '@angular/router';
 import Swal from 'sweetalert2';
+import * as CryptoJS from 'crypto-js';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-payment_preview_page',
@@ -13,7 +15,7 @@ import Swal from 'sweetalert2';
   providers: [MessageService],
 })
 export class Payment_preview_pageComponent implements OnInit {
-
+  secretKey = environment.secretKey
   memb_name : any
   form_no : any
   amount : any
@@ -28,11 +30,20 @@ export class Payment_preview_pageComponent implements OnInit {
   ) { }
 
     ngOnInit() {
+
     this.route.queryParams.subscribe(params => {
-      this.member_id = params['member_id']; 
-      this.memb_name = params['memb_name']; 
-      this.form_no = params['form_no']; 
-      this.amount = params['amount']; 
+      const enc_dt = params['enc_dt'];
+  
+      if (enc_dt) {
+        const decDt = CryptoJS.AES.decrypt(enc_dt, this.secretKey).toString(CryptoJS.enc.Utf8);
+  
+        const custDt = JSON.parse(decDt);
+  
+        this.member_id = custDt.member_id;
+        this.memb_name = custDt.memb_name;
+        this.form_no = custDt.form_no;
+        this.amount = custDt.amount;
+      }
     });
   }
 
